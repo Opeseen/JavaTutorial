f14df4762d10082f2c74628b50b87e87cfaea159
Opeseen <Opeyemi5055@yahoo.com>
2023-08-05 17:25:33 +0100

    First Java commit On NetBeans

---
+++ src/controlflow/ControlFlow.java
@@ -0,0 +1,24 @@
+package controlflow;
+
+import java.util.*;
+public class ControlFlow {
+
+    public static void main(String[] args) {
+        // IF STATEMENTS.
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Input Now: ");
+        String input = reader.next();
+        System.out.println(input);
+        System.out.print("Input Number: ");
+        int number = reader.nextInt();
+         
+        if(number > 10){
+            System.out.println(number);
+        }
+        if(number <= 10){
+            System.out.println("Number Less or Equal to Ten!");
+        }        
+         
+    }
+    
+}
