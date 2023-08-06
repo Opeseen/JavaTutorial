0da52b09c2aaff7ca06b9cc835a30858ee4556a9
Opeseen <Opeyemi5055@yahoo.com>
2023-08-06 21:43:01 +0100

    Deployed

---
+++ Switch.java
@@ -0,0 +1,18 @@
+public class Switch {
+  public static void main(String[] args){
+    int y = 10;
+    switch (y) {
+      case 1: case 6: case 10:
+      System.out.println("Red");
+      break;
+      case 2:
+      System.out.println("Yellow");
+      break;
+      case 3:
+      System.out.println("Blue");
+      break;
+      default:
+      System.out.println("None");
+    }
+  }
+}
