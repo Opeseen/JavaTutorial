520d717fd474751b65682faf117daa6835fc673c
Opeseen <Opeyemi5055@yahoo.com>
2023-09-22 18:05:46 +0100

    Deployed

--- classes.java
+++ classes.java
@@ -2,7 +2,6 @@ import java.util.*;
 public class classes{
   public static void main(String[] args){
     Scanner reader = new Scanner(System.in);
-    Person John = 
 
     System.out.println(reader.next());
   }
