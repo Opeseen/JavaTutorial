d3814d855d18a69b626406b0b844368eb56e7099
Opeseen <Opeyemi5055@yahoo.com>
2023-09-23 13:33:53 +0100

    Deployed

--- Streamint.java
+++ Streamint.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.OptionalInt;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -8,28 +10,35 @@ import java.util.stream.IntStream;
 public class Streamint{
   public static void main(String[] args){
    List<Integer> list = Arrays.asList(5, 5, 3, 4, 5);
-		Optional<Integer> result = list.stream().filter(num -> num < 5).findAny();
-		System.out.println("Result Is " + result);
+		// Optional<Integer> result = list.stream().filter(num -> num < 5).findAny();
+		// System.out.println("Result Is " + result);
 
-    IntStream stream = IntStream.range(100, 200);
+    // IntStream stream = IntStream.range(100, 200);
     // filter by number divisible by 5 and 7
-		System.out.println("numbers divisible by 5 and 7 are : ");
-		stream.filter(i -> (i % 5 == 0 && i % 7 == 0)).forEach(System.out::println);
+		// System.out.println("numbers divisible by 5 and 7 are : ");
+		// stream.filter(i -> (i % 5 == 0 && i % 7 == 0)).forEach();
 
-		Arrays.toString(IntStream.of(1, 2, 3, 1, 2, 3).distinct().toArray());
+		// Arrays.toString(IntStream.of(1, 2, 3, 1, 2, 3).distinct().toArray());
 
-		IntStream mapInput = IntStream.iterate(10, i -> i + 1)
-				.limit(5)
-				.map(i -> i * 2);
+		// IntStream mapInput = IntStream.iterate(10, i -> i + 1)
+		// 		.limit(5)
+		// 		.map(i -> i * 2);
 
-		System.out.println("map input stream : " + Arrays.toString(mapInput.toArray()));
+		// System.out.println("map input stream : " + Arrays.toString(mapInput.toArray()));
 
 		// IntStream mapOutput = mapInput.map( i -> i * 2);
 
 		// System.out.println("map Output stream : "+Arrays.toString(mapOutput.toArray()));
 
-    Set<Integer> set = IntStream.of(1, 2, 3, 1, 2, 3).boxed().collect(Collectors.toSet());
-    System.out.println("IntStream to Set : "+set);
+    // Set<Integer> set = IntStream.of(1, 2, 3, 1, 2, 3).boxed().collect(Collectors.toSet());
+    // System.out.println("IntStream to Set : "+set);
 
+		Integer[] integers = new Integer[] {1,2,3,4,5};
+		List<Integer> Listed = new ArrayList<>(Arrays.asList(integers));
+		// System.out.println(list22.get(4));
+		IntStream stream = IntStream.range(0, Listed.size());
+		// OptionalInt result = stream.filter(index -> Listed.get(index).equals(4)).findFirst();
+		OptionalInt result = stream.filter(index -> Listed.get(index).equals(5)).findFirst();
+		System.out.println("result is " + result);
   }
 }
